66ea89d8a43562c554b4346e279b89be8896ea37
Nícolas Castillo <ncastilloraggio@gmail.com>
2020-09-25 01:14:06 -0500

    Añadidas las paginas extra y arreglada vista movil

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('nav-menu').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');
 });
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 300});
+ScrollReveal().reveal('.cards-banner-one', {delay: 300});
+ScrollReveal().reveal('.cards-banner-two', {delay: 300});
+ScrollReveal().reveal('.social', {delay: 300});
+ScrollReveal().reveal('.footer-links', {delay: 300});
+ScrollReveal().reveal('.footer', {delay: 300});
